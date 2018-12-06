68a0bce32c90b8a0267512e207e0c05f35caeb33
Iacopo Ghinassi <45108036+Ighina@users.noreply.github.com>
2018-12-06 12:37:55 +0000

    Update app.R

--- app.R
+++ app.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("shinythemes", "shiny", "igraph", "tm", "tokenizers","stringr","readr")
+list.of.packages <- c("shinythemes", "shiny", "igraph", "tm", "tokenizers","stringr","readr","DT")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
